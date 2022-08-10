c1f7b4054a756188bd25a7a589f5a6944a71d5b8
Marcus Hellams <mhellams@hotmail.com>
2022-08-10 15:25:33 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [eslint(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/tests/setup.ts'],
+  },
+});
