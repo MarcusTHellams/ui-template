13d4477aa1991d379eb97ba9a7433c6cfce5f7a5
Marcus Hellams <mhellams@hotmail.com>
2023-08-13 19:56:18 -0400

    update integration of tailwind and chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Container, Heading } from '@chakra-ui/react';
+import { Container, Heading, Text } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
-      <div className={'prose container mx-auto mt-16'}>
-        <h1>Hello from App.</h1>
+      <div className={'prose max-w-none'}>
+        <Container mt="16" maxW="container.sm">
+          <Heading as="h1">Hello from App</Heading>
+          <Heading mt="0" as="h2">Hello from h2</Heading>
+          <Text>Paragraph Text</Text>
+          <Text fontFamily="mono">Paragraph Text</Text>
+        </Container>
       </div>
-      <Container mt="16" maxW="container.sm">
-        <Heading as="h1">Hello from App</Heading>
-      </Container>
     </>
   );
 }
