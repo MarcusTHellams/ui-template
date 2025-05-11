143f1f634d68e167f5bd6ee0ee51512126c85ce0
Marcus Hellams <mhellams@hotmail.com>
2025-05-11 14:57:22 -0400

    Update setup.ts
    add pointer events to the window

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -4,6 +4,10 @@ import { cleanup } from '@testing-library/react';
 
 import { server } from '../mocks/server';
 
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+
+
 beforeAll(() => {
   server.listen();
 });
